27740788cb8cf481525a88db90102ba4b6358a72
mubashshir96 <siddiquimubashshir791@gmail.com>
2025-12-27 22:42:21 +0530

    Refactor Firebase integration and authentication methods

--- app.js
+++ app.js
@@ -1,56 +1,54 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onChildAdded
+} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
 
+/* 🔴 APNA REAL FIREBASE CONFIG DALO */
 const firebaseConfig = {
- apiKey: "AIzaSyDArQkJaFoPMQeOoHi1LQPB2Umm4LS8oK8",
- authDomain: "to-1-chat-a9582.firebaseapp.com",
- databaseURL: "https://to-1-chat-a9582-default-rtdb.firebaseio.com",
- projectId: "to-1-chat-a9582"
+  apiKey: "AIzaSyDArQkJaFoPMQeOoHi1LQPB2Umm4LS8oK8",
+  authDomain: "to-1-chat-a9582.firebaseapp.com",
+  databaseURL: "https://to-1-chat-a9582-default-rtdb.firebaseio.com",
+  projectId: "to-1-chat-a9582"
 };
-firebase.initializeApp(firebaseConfig);
 
-const auth=firebase.auth();
-const db=firebase.database();
-let uid;
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
 
-function login(){
- auth.signInWithEmailAndPassword(email.value,password.value).then(u=>start(u.user.uid));
-}
-function register(){
- auth.createUserWithEmailAndPassword(email.value,password.value).then(u=>start(u.user.uid));
-}
+/* 🔹 GLOBAL FUNCTIONS (VERY IMPORTANT) */
+window.login = function () {
+  signInWithEmailAndPassword(auth, email.value, password.value)
+    .then(startChat)
+    .catch(err => alert(err.message));
+};
 
-function start(id){
- uid=id;
- login.style.display="none";
- chat.style.display="block";
- db.ref("online/"+uid).set(true);
- db.ref("messages").on("child_added",s=>{
-  const m=s.val();
-  if(m.type==="img") messages.innerHTML+=`<img src="${m.text}">`;
-  else messages.innerHTML+=`<div>${m.text}</div>`;
- });
-}
+window.register = function () {
+  createUserWithEmailAndPassword(auth, email.value, password.value)
+    .then(startChat)
+    .catch(err => alert(err.message));
+};
 
-function typing(){
- db.ref("typing/"+uid).set(true);
- setTimeout(()=>db.ref("typing/"+uid).remove(),1000);
+function startChat() {
+  login.style.display = "none";
+  chat.style.display = "block";
+
+  const msgRef = ref(db, "messages");
+  onChildAdded(msgRef, snap => {
+    messages.innerHTML += `<div>${snap.val().text}</div>`;
+  });
 }
 
-function sendMsg(){
- if(img.files[0]){
-  const reader=new FileReader();
-  reader.onload=()=>db.ref("messages").push({text:reader.result,type:"img"});
-  reader.readAsDataURL(img.files[0]);
- }else{
-  db.ref("messages").push({text:msg.value,type:"text"});
- }
- msg.value="";
- img.value="";
-}
-
-// Simple WebRTC demo
-let pc=new RTCPeerConnection();
-async function startCall(){
- const stream=await navigator.mediaDevices.getUserMedia({video:true,audio:true});
- localVideo.srcObject=stream;
- stream.getTracks().forEach(t=>pc.addTrack(t,stream));
-}
+window.sendMsg = function () {
+  push(ref(db, "messages"), {
+    text: msg.value
+  });
+  msg.value = "";
+};
