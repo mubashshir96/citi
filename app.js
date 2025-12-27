f5436a04dc3d890974d29e22dea70050abc0d3f0
mubashshir96 <siddiquimubashshir791@gmail.com>
2025-12-27 23:13:15 +0530

    Update Firebase API key in app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import { getDatabase, ref, push, onChildAdded, set, remove, onDisconnect }
 from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDArQkJaFoPMQeOoHi1LQPB2Umm4LS8oK8Y",
+  apiKey: "AIzaSyDArQkJaFoPMQeOoHi1LQPB2Umm4LS8oK8",
   authDomain: "to-1-chat-a9582.firebaseapp.com",
   databaseURL: "https://to-1-chat-a9582-default-rtdb.firebaseio.com",
   projectId: "to-1-chat-a9582"
